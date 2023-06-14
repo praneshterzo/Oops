22b540092290579487b3abad4136d5d6d030654d
praneshterzo <pranesh@terzocloud.com>
2023-06-14 16:54:01 +0530

    commit1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
-public class Main{
-    public int a,b;
-    public Main(int x,int y){
-        this.a= x;
-        this.b=y;
+class Student {
+    String name ;
+    int age;
+    String address;
+
+    public Student(String name, int age, String address){
+        this.name=name;
+        this.age=age;
+        this.address=address;
     }
-    public int add(){
-        return a+b;
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setAge(int age){
+        this.age=age;
+    }
+    public void setAddress(String address){
+        this.address=address;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String toString(){
+        return "Student name : "+name+"\nAge : "+age+"\nAddress : "+address;
+    }
+
+    public static void main(String[] args) {
+        Student Daniel = new Student("Daniel", 20,"nowhere");
+        Daniel.setName("Pranesh");
+        Daniel.setAge(15);
+        System.out.println(Daniel.getName());
+        System.out.println(Daniel.toString());;
     }
 }
