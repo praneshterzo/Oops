25a8e12d55f5c5a87dbb9cd59ce0f38b2a1b0404
praneshterzo <pranesh@terzocloud.com>
2023-06-14 16:43:26 +0530

    commit1

---
+++ src/operations.java
@@ -0,0 +1,11 @@
+public class operations extends Main {
+    public operations(int a,int b){
+        super(a,b);
+        this.a=a;
+        this.b=b;
+    }
+    public int sub() {
+        return a - b;
+    }
+
+}
