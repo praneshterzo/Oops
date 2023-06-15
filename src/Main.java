71fd8218d90f5102e3adc7cc0cc5f863500673b1
praneshterzo <pranesh@terzocloud.com>
2023-06-15 10:25:11 +0530

    commit6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-public class Main{
-    public static void main(String[] args) {
-        Bike harley = new Bike(10,1, 50);
-        harley.speedUp(1);
-        System.out.println(harley.getSpeed());
+
+abstract class pranesh{
+    abstract void name();
+
+    public void type(){
+        System.out.print("Employee");
+    }
+}
+
+class terzo extends pranesh{
+
+    @Override
+    void name() {
+        System.out.print("Terzo ");
+    }
+
+}
+
+public class Main {
+
+    public static void main(String args[]){
+
+        pranesh p = new terzo();
+        p.name();
+        p.type();
     }
 }
